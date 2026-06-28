e1aba8a3b5eda768ea11463e5440e390095156c4
CyberRift01 <likhon30122009@gmail.com>
2026-06-28 13:53:46 +0600

    Export topic and graph schemas from index.ts
    I have to exported it.

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -2,3 +2,16 @@ export * from './subject'
 export * from './guide'
 export * from './identity'
 export * from './learning-path'
+export {
+  createTopicSchema,
+  type CreateTopicInput,
+} from './topic'
+
+export {
+  createPrerequisiteSchema,
+  deletePrerequisiteSchema,
+  createTodoPrerequisiteSchema,
+  type CreatePrerequisiteInput,
+  type DeletePrerequisiteInput,
+  type CreateTodoPrerequisiteInput,
+} from './graph'
